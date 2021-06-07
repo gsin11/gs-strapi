fb5020c565b45d12bffed91aa63ca4a3d5a52cf9
Gurpreet Singh <gurpreet.singh7@publicissapient.com>
2021-06-07 19:47:06 +0200

    inital commit

---
+++ config/env/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "af7d7c9482f368780058db87926d5507"),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1538b679-eac0-4a1f-ad61-5376a3856f0c'
+};
